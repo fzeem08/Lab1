b07d4118e2e4afae3859feb741147d48625eaf9c
Fyzul Shams Khan Zeem <156142043+fzeem08@users.noreply.github.com>
2024-02-09 15:26:48 -0500

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,10 @@
+# Lab1/script.py
+
+def calculate_age():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = 2024  # You can update this to the current year
+    age = current_year - birth_year
+    return age
+
+# Test the function
+print("Your age is:", calculate_age())
