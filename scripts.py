1f102ba01d4bb88007c5a7e34b2c5c9128b670a7
Fyzul Shams Khan Zeem <156142043+fzeem08@users.noreply.github.com>
2024-02-09 16:55:45 -0500

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,14 +1,19 @@
 # Lab1/script.py
 
-def calculate_age(22):
-    birth_year = int(input("Enter your birth year: "))
-    current_year = 2024  # You can update this to the current year
+def calculate_age():
+    try:
+        birth_year = int(input("Enter your birth year: "))
+    except ValueError:
+        print("Please enter an integer for the birth year.")
+        return
+
+    current_year = 2024
     age = current_year - birth_year
     return age
 
 def helloWorld():
     print('Hello World')
 
-# Test the function
+# Test the functions
 print("Your age is:", calculate_age())
 helloWorld()
